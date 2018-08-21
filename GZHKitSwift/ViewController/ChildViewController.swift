966b41df906dc264f4beb41449144264a7823bc4
o(￣へ￣)o <514668826@qq.com>
2018-08-21 11:01:42 +0800

    add childviewcontroller extension

---
+++ GZHKitSwift/ViewController/ChildViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChildViewController.swift
+//  RealtyNavi
+//
+//  Created by 顾振华 on 2018/8/21.
+//  Copyright © 2018年 LanSi. All rights reserved.
+//
+
+import Foundation
+
+extension UIViewController {
+    func addComponentViewController(_ viewController: UIViewController) {
+        addComponentViewController(viewController, at: view)
+    }
+
+    func addComponentViewController(_ viewController: UIViewController, at containerView: UIView) {
+        addChildViewController(viewController)
+        containerView.addSubview(viewController.view)
+        viewController.didMove(toParentViewController: self)
+    }
+    
+    func removeComponented() {
+        guard parent != nil else {
+            return
+        }
+        willMove(toParentViewController: nil)
+        removeFromParentViewController()
+        view.removeFromSuperview()
+    }
+}
