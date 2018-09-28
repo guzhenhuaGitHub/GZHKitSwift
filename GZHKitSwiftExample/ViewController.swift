b494714c215cc206c38ec0e05278eb31fe3a673d
o(￣へ￣)o <514668826@qq.com>
2018-09-28 13:40:45 +0800

    em...

--- GZHKitSwiftExample/ViewController.swift
+++ GZHKitSwiftExample/ViewController.swift
@@ -14,10 +14,5 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
 
-        let sliderBar = UISlider()
-        sliderBar.frame = CGRect(x: 100, y: 100, width: 200, height: 100)
-        sliderBar.setThumbImage(#imageLiteral(resourceName: "slider"), for: .normal)
-        view.addSubview(sliderBar)
-
     }
 }
