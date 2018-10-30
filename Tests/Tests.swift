0c3e5e7aafb076d0f0e75d7346fb7be7c3959b8e
o(￣へ￣)o <514668826@qq.com>
2018-10-30 10:09:04 +0800

    test

--- Tests/Tests.swift
+++ Tests/Tests.swift
@@ -19,7 +19,7 @@ class GZHKitSwiftTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testRegularExpression() {
         XCTAssert("regex" =~ "^r")
     }
 }
